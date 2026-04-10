314dc9df6c0ce771a5a99129e51e7183987ea6a3
André Moreira <andrem.33333@gmail.com>
2026-04-10 05:37:18 -0300

    fix: resolve conflict in ppu.h

--- ppu.h
+++ ppu.h
@@ -5,23 +5,16 @@
 #ifndef PPU_H
 #define PPU_H
 
-<<<<<<< fix-ppu-crash
 #include "common.h"
 
 #define MAX_PIXELS (256 * 240 * 2)
 
-=======
->>>>>>> master
 extern byte PPU_SPRRAM[0x100];
 extern byte PPU_RAM[0x4000];
 
 
 // Pixels to draw
-<<<<<<< fix-ppu-crash
 extern ALLEGRO_VERTEX ppu_background_pixels[MAX_PIXELS], ppu_sprite_pixels[MAX_PIXELS], ppu_behind_background_sprite_pixels[MAX_PIXELS];
-=======
-extern ALLEGRO_VERTEX ppu_background_pixels[65536], ppu_sprite_pixels[65536], ppu_behind_background_sprite_pixels[65536];
->>>>>>> master
 
 // Count of pixels to draw
 extern int ppu_background_pixels_number, ppu_behind_background_sprite_pixels_number, ppu_sprite_pixels_number;
